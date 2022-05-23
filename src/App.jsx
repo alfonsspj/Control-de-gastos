8bfd315e7324743bbcc353973b0b8d46ee0f6a56
alfons <alfonsspj@gmail.com>
2022-05-22 23:02:49 -0300

    Filtrar Gastos: Categorias, mostrar Gastos Filtrados

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Filtros from './components/Filtros'
 import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
 import { generarId } from './helpers';
@@ -18,6 +19,8 @@ function App() {
   const [animarModal, setAnimarModal] = useState(false);
 
   const [gastoEditar, setGastoEditar] = useState({});
+  const [filtro, setFiltro] = useState('');
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
 
   useEffect(() => {
     if( Object.keys(gastoEditar).length > 0 ) {
@@ -47,6 +50,17 @@ function App() {
       setIsValidPresupuesto(true)
     }
   }, [])
+
+  // escucha los cambios que sucedan en filtro
+  useEffect(() => {
+    if(filtro) {
+      // Filtrar gastos por categoria
+      const gastosFiltrados = gastos.filter( gasto => gasto.categoria === filtro);
+
+      setGastosFiltrados(gastosFiltrados)
+
+    }
+  }, [filtro]);
 
   const handleNuevoGasto = () => {
     setModal(true)
@@ -99,10 +113,16 @@ function App() {
       {isValidPresupuesto && (
         <>
           <main>
+            <Filtros
+              filtro={filtro}
+              setFiltro={setFiltro}
+            />
             <ListadoGastos
               gastos={gastos}
               setGastoEditar={setGastoEditar}
               eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
             />
           </main>
           <div className="nuevo-gasto">
